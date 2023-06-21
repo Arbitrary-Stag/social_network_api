c46b70564fc69377704d05555be77fa67590571b
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-06-20 17:56:10 -0700

    Created controller folder, userController.js file, wrote code for User CRUD, updated user api routes.

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const routes = require('./routes');
 const app = express();
 const PORT = 3001;
 
+// Built in Express function that parses incoming requests to JSON
 app.use(express.json());
 app.use(express.urlencoded({ extened: true }));
 app.use(routes);
 
 db.once('open', () => {
+  // start up express server
   app.listen(PORT, () => {
     console.log(`API server for social network running on port ${PORT}!`);
   });
